ec17f06de410e760ed4bf067606d4f6e1dd67222
Aditya <adityaagupta31@gmail.com>
2022-02-14 18:58:00 +0530

    link added

---
+++ src/components/helpers/Fork.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaGithubAlt } from "react-icons/fa";
+
+interface ForkProps {}
+
+export const Fork: React.FC<ForkProps> = ({}) => {
+  return (
+    <div className="ribbon">
+      <a href="https://github.com/open-certs/oc-frontend" target={"_blank"}>
+        Fork me on
+        <FaGithubAlt size={20} style={{ marginLeft: "0.5em" }} />
+      </a>
+    </div>
+  );
+};
