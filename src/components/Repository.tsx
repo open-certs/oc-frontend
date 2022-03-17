b889ea4c68bb1d8d0fa075e4cff09bd06ce8b6f0
shelabh <68127067+shelabh@users.noreply.github.com>
2022-03-17 11:53:20 +0530

    completed repository component and made it responsive

--- src/components/Repository.tsx
+++ src/components/Repository.tsx
@@ -2,44 +2,84 @@ import React from "react";
 
 const Repository = () => {
   return (
-    <div className="w-full">
-      <div className="w-full flex flex-col  text-primary-100  ml-[5%] space-y-10">
-        <div className="w-full  ">
-          <div className="text-6xl">oc-frontend</div>
-          <p className="text-lg mt-3">
+    <div className="w-full h-full">
+      <div className=" flex  lg:flex-row flex-col  text-primary-100  lg:ml-[5%] lg:space-x-5 lg:mb-5">
+        <div className="border rounded lg:w-[30%] h-[13rem] flex flex-col  items-center">
+          <a
+            href="https://github.com/open-certs/oc-frontend"
+            className="lg:text-4xl text-2xl mt-2"
+          >
+            oc-frontend
+          </a>
+          <p className="text-lg mt-3 ml-4">
             An Open-Source Platform to certify open-source projects.
           </p>
-          <div className="flex">
-            <div>Typescript</div>
-            <div>
-              <img />
-              <span>10</span>
+          <div className="flex mt-3 space-x-5 ml-2 lg:ml-0">
+            <div className="flex space-x-2 ">
+              <img src="/images/blue.svg" className="w-[15%] lg:w-full" />
+              <span className="lg:mt-[2%] mt-[13%]">Typescript</span>
             </div>
-            <div>
-              <img />
-              <span>17</span>
+            <div className="flex space-x-2">
+              <img src="/images/star.svg" className="w-[50%] lg:w-full" />
+              <span className="lg:mt-[10%] mt-[40%]">10</span>
             </div>
-            <div>Updated 3 days ago</div>
+            <div className="flex space-x-2">
+              <img src="/images/fork.svg" className="w-[45%] lg:w-full" />
+              <span className="lg:mt-[10%] mt-[40%]">17</span>
+            </div>
+            <span className="mt-1">Updated 3 days ago</span>
           </div>
         </div>
-        <div>
-          <div>Topic</div>
-          <p>Description</p>
-          <div>
-            <div>Typescript</div>
-            <div>Stars</div>
-            <div>Fork</div>
-            <div>Time</div>
+        <div className="border rounded lg:w-[30%] lg:mt-0 w-full h-[13rem] flex flex-col mt-5  items-center">
+          <a
+            href="https://github.com/open-certs/oc-backend"
+            className="lg:text-4xl text-2xl mt-2"
+          >
+            oc-backend
+          </a>
+          <p className="text-lg mt-3 ml-4 lg:ml-0">
+            This includes the backend server for Open-Certs.
+          </p>
+          <div className="flex mt-3 space-x-5 ml-2 lg:ml-0">
+            <div className="flex space-x-2">
+              <img src="/images/blue.svg" className="w-[15%] lg:w-full" />
+              <span className="lg:mt-[2%] mt-[13%]">Typescript</span>
+            </div>
+            <div className="flex space-x-2">
+              <img src="/images/star.svg" className="w-[50%] lg:w-full" />
+              <span className="lg:mt-[10%] mt-[40%]">6</span>
+            </div>
+            <div className="flex space-x-2">
+              <img src="/images/fork.svg" className="w-[45%] lg:w-full" />
+              <span className="lg:mt-[10%] mt-[40%]">16</span>
+            </div>
+            <span className="mt-1">Updated 3 days ago</span>
           </div>
         </div>
-        <div>
-          <div>Topic</div>
-          <p>Description</p>
-          <div>
-            <div>Typescript</div>
-            <div>Stars</div>
-            <div>Fork</div>
-            <div>Time</div>
+        <div className="border rounded lg:w-[30%] lg:mt-0 w-full h-[13rem] flex flex-col mt-5  items-center">
+          <a
+            href="https://github.com/open-certs/oc-truffle"
+            className="lg:text-4xl text-2xl mt-2"
+          >
+            oc-truffle
+          </a>
+          <p className="text-lg mt-3 ml-4 lg:ml-0">
+            This includes the solidity code for Open-Certs.
+          </p>
+          <div className="flex mt-3 space-x-5 ml-2 lg:ml-0">
+            <div className="flex space-x-2">
+              <img src="/images/blue.svg" className="w-[15%] lg:w-full" />
+              <span className="lg:mt-[2%] mt-[13%]">Typescript</span>
+            </div>
+            <div className="flex space-x-2">
+              <img src="/images/star.svg" className="w-[50%] lg:w-full" />
+              <span className="lg:mt-[10%] mt-[40%]">4</span>
+            </div>
+            <div className="flex space-x-2">
+              <img src="/images/fork.svg" className="w-[45%] lg:w-full" />
+              <span className="lg:mt-[10%] mt-[40%]">2</span>
+            </div>
+            <span className="mt-1">Updated 3 days ago</span>
           </div>
         </div>
       </div>
